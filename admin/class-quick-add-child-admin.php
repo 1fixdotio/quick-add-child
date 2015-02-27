4cbe22de7afb6d5e9b1325604fab7edde948c84f
Yoren Chang <yorenchang@gmail.com>
2015-02-27 11:34:49 +0800

    Enqueue and localize scripts only on post editing screen

--- admin/class-quick-add-child-admin.php
+++ admin/class-quick-add-child-admin.php
@@ -114,17 +114,17 @@ class Quick_Add_Child_Admin {
 
 		if ( 'post.php' == $hook && is_post_type_hierarchical( $post->post_type ) ) {
 			wp_enqueue_script( $this->plugin_slug . '-admin-script', plugins_url( 'assets/js/admin.js', __FILE__ ), array( 'jquery' ), Quick_Add_Child::VERSION, true );
+
+			$options = get_option( $this->plugin_slug );
+			$params = array(
+				'add_new_child' => __( 'Add New Child', 'quick-add-child' ),
+				'add_new_sibling' => __( 'Add New Sibling', 'quick-add-child' ),
+				'hide_add_new' => ( isset( $options['hide_add_new'] ) ) ? $options['hide_add_new'] : 'off',
+				);
+			$params = apply_filters( 'quick_add_child_js_params', $params );
+
+			wp_localize_script( $this->plugin_slug . '-admin-script', 'quick_add_child_js_params', $params );
 		}
-
-		$options = get_option( $this->plugin_slug );
-		$params = array(
-			'add_new_child' => __( 'Add New Child', 'quick-add-child' ),
-			'add_new_sibling' => __( 'Add New Sibling', 'quick-add-child' ),
-			'hide_add_new' => ( isset( $options['hide_add_new'] ) ) ? $options['hide_add_new'] : 'off',
-			);
-		$params = apply_filters( 'quick_add_child_js_params', $params );
-
-		wp_localize_script( $this->plugin_slug . '-admin-script', 'quick_add_child_js_params', $params );
 
 	}
 
